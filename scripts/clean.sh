b3da41ba53d0fe11afeb2b873f96d7e9c9f16620
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2015-12-03 22:48:58 +0100

    Updated make clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,7 +2,12 @@
 
 cd $WORKSPACE_ROOT
 
-# Confirmation prompt before deleting?
-
-catkin clean -a #"$@"
-
+echo -n "Do you want to clean devel and build? [y/n] "
+read -N 1 REPLY
+echo
+if test "$REPLY" = "y" -o "$REPLY" = "Y"; then
+  catkin clean --all
+  echo " >>> Cleaned devel and build directories."
+else
+  echo " >>> Clean cancelled by user."
+fi
